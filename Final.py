50db2ff2ae24136295672aba032e01d15bf23a2b
glenpassow <glenpassow@hanovernorwichschools.org>
2016-05-13 14:26:19 -0400

    Updated from Brython Server: May 13, 2016 2:26:19 PM EDT

--- Final.py
+++ Final.py
@@ -39,7 +39,7 @@ for x in range(0, height):
         yshift = height*celld + 20
         Sprite(rectangle2, (x*celld, y*celld + yshift))
         oceanself[(x,y)] = cell(rectangle, (x*celld, y*celld + yshift))
-        selfBoats[(x,y)] = cell(rectangle3, (x*celld, y*celld + yshift))
+        #selfBoats[(x,y)] = cell(rectangle3, (x*celld, y*celld + yshift))
 
 for a in range(0, 4):
     randx = randrange(1, 10)
