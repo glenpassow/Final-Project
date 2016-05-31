118e6586ee46ec8b1005b36e618f8d92c2e2bd7c
glenpassow <glenpassow@hanovernorwichschools.org>
2016-05-31 14:22:33 -0400

    Updated from Brython Server: May 31, 2016 2:22:33 PM EDT

--- Final.py
+++ Final.py
@@ -94,7 +94,7 @@ class Battleship(App):
         self.cy = int(event.y/celld)
         ocean[(self.cx, self.cy)].visible = False
         #oceanself[(self.cx, self.cy)].visible = False
-        selfBoats[(self.cx+yshift,self.cy+yshift)].visible = True
+        #selfBoats[(self.cx+yshift,self.cy+yshift)].visible = True
         if (self.cx, self.cy) in enemyboatsalive and (self.cx, self.cy) not in enemyboatsunk:
             self.squarehit = self.squarehit + 1
             enemyboatsunk[self.cx, self.cy] = "hit"
